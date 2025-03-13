b35a68b0e8e7aa7cf139dfcfb342c3a03d306a11
Serafin Kaiser <serafinkaiser@yahoo.de>
2025-03-14 00:54:53 +0100

    Added string utilities

--- reframework/autorun/MHWCrownHelper/Utils.lua
+++ reframework/autorun/MHWCrownHelper/Utils.lua
@@ -1,10 +1,10 @@
 local Singletons = require "MHWCrownHelper.Singletons"
 local Const      = require "MHWCrownHelper.Const"
-local Utils = {};
+local Utils      = {};
 
 local nameString = "[MHWCrownHelper]: ";
 
-Utils.debugMode = true;
+Utils.debugMode  = true;
 
 --[[ Logging ]] --
 -------------------------------------------------------------------
@@ -236,6 +236,56 @@ function Utils.GetFontD2D(key, size)
     end
 
     return nil;
+end
+
+-------------------------------------------------------------------
+
+---@param self string
+function string:contains(sub)
+    return self:find(sub, 1, true) ~= nil
+end
+
+-------------------------------------------------------------------
+
+---@param self string
+function string:startswith(start)
+    return self:sub(1, #start) == start
+end
+
+-------------------------------------------------------------------
+
+---@param self string
+function string:endswith(ending)
+    return ending == "" or self:sub(- #ending) == ending
+end
+
+-------------------------------------------------------------------
+
+---@param self string
+function string:replace(old, new)
+    local s = self
+    local search_start_idx = 1
+
+    while true do
+        local start_idx, end_idx = s:find(old, search_start_idx, true)
+        if (not start_idx) then
+            break
+        end
+
+        local postfix = s:sub(end_idx + 1)
+        s = s:sub(1, (start_idx - 1)) .. new .. postfix
+
+        search_start_idx = -1 * postfix:len()
+    end
+
+    return s
+end
+
+-------------------------------------------------------------------
+
+---@param self string
+function string:insert(pos, text)
+    return self:sub(1, pos - 1) .. text .. self:sub(pos)
 end
 
 return Utils;
